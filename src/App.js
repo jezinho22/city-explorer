e7f709b70d1a21ab5c82426d0e420fa70add9421
Jeremy Johns <jez.johns@gmail.com>
2023-05-09 16:05:39 +0000

    completed lab06

--- src/App.js
+++ src/App.js
@@ -5,25 +5,34 @@ import "./App.css";
 function App() {
 	const [searchQuery, setsearchQuery] = useState("");
 	const [location, setLocation] = useState({});
+	const [mapImg, setmapImg] = useState("");
 
 	async function getLocality(event) {
 		event.preventDefault();
+		// get the main location data
 		let API = `https://eu1.locationiq.com/v1/search?key=${process.env.REACT_APP_API_KEY}&q=${searchQuery}&format=json`;
+
 		try {
+			// get location data
 			const result = await axios.get(API);
 			setLocation(result.data[0]);
+			// create location map url
+			setmapImg(
+				`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${location.lat},${location.lon}&zoom=15`
+			);
 		} catch (error) {
-			console.log("The error is " + error);
+			// error message
+			alert("It looks like what you entered is not a placename");
 		}
-		console.log(location);
 	}
+	// get the string from input
 	function handleChange(event) {
 		setsearchQuery(event.target.value);
 	}
+
 	return (
 		<div className="App">
 			<h1>Location Explorer</h1>
-			<h2>{searchQuery}</h2>
 			<form>
 				<fieldset>
 					<legend>Input a location</legend>
@@ -33,10 +42,17 @@ function App() {
 					</button>
 				</fieldset>
 			</form>
+			{location && (
+				<div>
+					<h3>{location.display_name}</h3>
+					<div>Latitude: {location.lat}</div>
+					<div>Longitude: {location.lon}</div>
+				</div>
+			)}
 
-			<div>Name: {location.display_name}</div>
-			<div>Latitude: {location.lat}</div>
-			<div>Longitude: {location.lon}</div>
+			<div className="mapDisplay">
+				{mapImg && <img src={mapImg} alt="map"></img>}
+			</div>
 		</div>
 	);
 }
