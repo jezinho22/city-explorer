296ac830dd9cdb48a9b5912ed63110f1454e2350
Jeremy Johns <jez.johns@gmail.com>
2023-05-11 20:51:25 +0000

    working but throwing error because of lag

--- src/App.js
+++ src/App.js
@@ -9,18 +9,28 @@ function App() {
 	const [mapImg, setmapImg] = useState("");
 	const [weather, setWeather] = useState([]);
 
-	async function getLocality(event) {
+	async function getLocation() {
+		let API = `https://eu1.locationiq.com/v1/search?key=${process.env.REACT_APP_API_KEY}&q=${searchQuery}&format=json`;
+		const result = await axios.get(API);
+		return result.data[0];
+	}
+	async function getWeather(location) {
+		// get weather
+		const weatherAPI = `http://localhost:8080/weather?lat=${location.lat}&lon=${location.lon}&searchQuery=${searchQuery}`;
+		const weatherResults = await axios.get(weatherAPI);
+		return weatherResults.data;
+	}
+	async function handleSubmit(event) {
 		event.preventDefault();
 		// get the main location data
-		let API = `https://eu1.locationiq.com/v1/search?key=${process.env.REACT_APP_API_KEY}&q=${searchQuery}&format=json`;
 
 		try {
 			// get location data
-			const result = await axios.get(API);
-			setLocation(result.data[0]);
+			setLocation(await getLocation());
+			console.log(location.lon, location.lat);
 			// stuck here
-			console.log(getWeather(location));
-
+			setWeather(await getWeather(location));
+			console.log(weather);
 			// create location map url
 			setmapImg(
 				`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${location.lat},${location.lon}&zoom=15`
@@ -29,13 +39,6 @@ function App() {
 			// error message
 			alert(error + ": It looks like what you entered is not a placename");
 		}
-	}
-	async function getWeather(location) {
-		// get weather
-		const weatherAPI = `http://localhost:8080/weather?lat=${location.lat}&lon=${location.lon}&searchQuery=${searchQuery}`;
-		const weatherResults = await axios.get(weatherAPI);
-		return weatherResults;
-		// setWeather(weatherResults.data);
 	}
 
 	// get the string from input
@@ -51,17 +54,15 @@ function App() {
 					<fieldset>
 						<legend>Input a location</legend>
 						<input onChange={handleChange} type="text" id="locationInput" />
-						<button onClick={getLocality}>Find me!</button>
+						<button onClick={handleSubmit}>Find me!</button>
 					</fieldset>
 				</form>
 				{location && (
 					<div>
 						<h3>{location.display_name}</h3>
-						{/* <div>Latitude: {location.lat}</div>
-						<div>Longitude: {location.lon}</div> */}
 					</div>
 				)}
-				{weather && <h4>Local weather</h4>}
+				{weather.length && <h4>Local weather</h4>}
 				{weather.map((day) => (
 					<Weather weather={day} />
 				))}
